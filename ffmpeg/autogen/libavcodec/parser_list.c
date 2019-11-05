945f4e618ef41e041d600f4bd00ab3630cedd6e0
Dima <yudind@gmail.com>
2019-11-05 16:53:11 +0300

    stick to ffmpeg-builder

--- ffmpeg/autogen/libavcodec/parser_list.c
+++
@@ -1,5 +0,0 @@
-static const AVCodecParser * const parser_list[] = {
-    &ff_aac_parser,
-    &ff_aac_latm_parser,
-    &ff_mpegaudio_parser,
-    NULL };
--- ffmpeg/autogen/libavformat/demuxer_list.c
+++
@@ -1,7 +0,0 @@
-static const AVInputFormat * const demuxer_list[] = {
-    &ff_aac_demuxer,
-    &ff_loas_demuxer,
-    &ff_mov_demuxer,
-    &ff_mpegts_demuxer,
-    &ff_mpegtsraw_demuxer,
-    NULL };
